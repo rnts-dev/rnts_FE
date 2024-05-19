56a61e44b5af94f722979ada49fe467ba5fcc247
jungjihyoun <jihyoun0602@gmail.com>
2024-05-19 11:45:34 +0900

    fix: 키 환경변수 처리

--- src/features/auth/kakao/ui/KakaoBtn.tsx
+++ src/features/auth/kakao/ui/KakaoBtn.tsx
@@ -2,9 +2,11 @@ import { Link } from 'react-router-dom';
 import kakaoLoginImage from '@/assets/kakaoLogin.png';
 import './kakaoBtn.scss';
 
+const KAKAKO_CLIENT_KEY = import.meta.env.KAKAO_CLIENT_KEY;
+
 const KakaoBtn = () => {
   return (
-    <Link to="https://kauth.kakao.com/oauth/authorize?response_type=code&redirect_uri=http://localhost:8080/kakao-redirect&client_id=3791f336642b43c859459e063130cb5a">
+    <Link to={`https://kauth.kakao.com/oauth/authorize?response_type=code&redirect_uri=http://localhost:8080/kakao-redirect&client_id=${KAKAKO_CLIENT_KEY}`}>
       <img className="container" src={kakaoLoginImage} />
     </Link>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log(env);
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            kakaoAppKey: env.VITE_KAKAO_APP_KEY,
+          },
+        },
+      }),
+    ],
+  };
 });
