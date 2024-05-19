6c74d9b0597f44df2652b75e184a32b38eb008ae
jungjihyoun <jihyoun0602@gmail.com>
2024-05-19 21:39:43 +0900

    feature: preview page 퍼블 수정

--- src/pages/preview/PreviewPage.tsx
+++ src/pages/preview/PreviewPage.tsx
@@ -1,12 +1,29 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import RNTSDatePicker from '@/shared/components/RNTSDatePicker/RNTSDatePicker';
-import { Divider } from '@chakra-ui/react';
 import './preview.scss';
 
 export default function PreviewPage() {
   return (
     <div className="preview-page">
-      <RNTSDatePicker onSave={() => console.log('날짜 선택')} />
-      <Divider />
+      <Tabs>
+        <TabList>
+          <Tab>DatePicker</Tab>
+          <Tab>timeline</Tab>
+          <Tab>..</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <RNTSDatePicker onSave={() => console.log('날짜 선택')} />
+          </TabPanel>
+          <TabPanel>
+            <p>timeline component</p>
+          </TabPanel>
+          <TabPanel>
+            <p>..!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 }
