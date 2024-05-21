ba14f2970a4ba504c32b9cd27a5549a8ad7d062a
jungjihyoun <jihyoun0602@gmail.com>
2024-05-21 19:28:50 +0900

    build

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,5 +1,12 @@
+import { testApi } from '@/shared/service/test';
+import { Button } from '@chakra-ui/react';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Button type="button" onClick={() => testApi('api 테스트 입니다')}>
+      로그인
+    </Button>
+  );
 };
 
 export default LoginPage;
