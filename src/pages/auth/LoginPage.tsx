7efd95fae48b5c2b85bc02e85780363e692a3502
jungjihyoun <jihyoun0602@gmail.com>
2024-05-21 20:03:40 +0900

    feature: nav bar

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -3,8 +3,8 @@ import { Button } from '@chakra-ui/react';
 
 const LoginPage = () => {
   return (
-    <Button type="button" onClick={() => testApi('api 테스트 입니다')}>
-      로그인
+    <Button type="button" colorScheme="teal" size="sm" onClick={() => testApi('api 테스트 입니다')}>
+      로그인 (서버 통신 테스트용. 눌러보세여)
     </Button>
   );
 };
