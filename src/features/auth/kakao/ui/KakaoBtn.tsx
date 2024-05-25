3b93120be93a754cb32cf60de9176e77d933a5ce
jungjihyoun <jihyoun0602@gmail.com>
2024-05-26 02:21:46 +0900

    fix : build error

--- src/features/auth/kakao/ui/KakaoBtn.tsx
+++ src/features/auth/kakao/ui/KakaoBtn.tsx
@@ -5,7 +5,9 @@ const KAKAKO_CLIENT_KEY = import.meta.env.VITE_KAKAO_CLIENT_KEY;
 
 const KakaoBtn = () => {
   return (
-    <Link className="kakao_login_btn" to={`https://kauth.kakao.com/oauth/authorize?response_type=code&redirect_uri=http://localhost:8080/login/oauth2/code/kakao&client_id=${KAKAKO_CLIENT_KEY}`}>
+    <Link
+      className="kakao_login_btn"
+      to={`https://kauth.kakao.com/oauth/authorize?response_type=code&redirect_uri=https://kauth.kakao.com/oauth/authorize/login/oauth2/code/kakao&client_id=${KAKAKO_CLIENT_KEY}`}>
       카카오로 로그인하기
     </Link>
   );
