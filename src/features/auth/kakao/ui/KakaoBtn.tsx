d713a8a4d6e9d6a410efe02801464ecf427445a8
jungjihyoun <jihyoun0602@gmail.com>
2024-05-26 02:35:22 +0900

    fix : kakao redirect url

--- src/features/auth/kakao/ui/KakaoBtn.tsx
+++ src/features/auth/kakao/ui/KakaoBtn.tsx
@@ -7,7 +7,8 @@ const KakaoBtn = () => {
   return (
     <Link
       className="kakao_login_btn"
-      to={`https://kauth.kakao.com/oauth/authorize?response_type=code&redirect_uri=https://kauth.kakao.com/oauth/authorize/login/oauth2/code/kakao&client_id=${KAKAKO_CLIENT_KEY}`}>
+      to={`https://kauth.kakao.com/oauth/authorize?response_type=code&redirect_uri=https://rnts-mzz5vukox-jungjihyouns-projects.vercel.app
+      /login/oauth2/code/kakao&client_id=${KAKAKO_CLIENT_KEY}`}>
       카카오로 로그인하기
     </Link>
   );
