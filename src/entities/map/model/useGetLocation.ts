964eac0238b8cbf2c3c35ef8934d0451f615b76d
striker1826 <alstjq123579@daum.net>
2024-05-25 15:53:39 +0900

    도착 지점과 현재 위치를 위, 경도로 계산하여 km 단위로 표현

--- src/entities/map/model/useGetLocation.ts
+++ src/entities/map/model/useGetLocation.ts
@@ -1,9 +1,14 @@
+import { calculrateDistance } from '@/shared/utils/calculator';
 import { useEffect, useState } from 'react';
 
 interface ILocation {
   latitude: string | number;
   longitude: string | number;
 }
+
+// TODO: test용 mockData 입니다. 도착지점의 위, 경도라고 생각해주시면 됩니다.
+const lat1 = 37.49808633653005; //위도
+const lng1 = 127.02800140627488; //경도
 
 export default function useGetLocation() {
   const [location, setLocation] = useState<ILocation>({ latitude: '', longitude: '' });
@@ -15,6 +20,7 @@ export default function useGetLocation() {
   const successHandler = (response: GeolocationPosition) => {
     const { latitude, longitude } = response.coords;
     setLocation({ latitude, longitude });
+    calculrateDistance(latitude, longitude, lat1, lng1);
   };
 
   const errorHandler = (error: GeolocationPositionError) => {
