986e614dc744c106aaf7f043102160f91edefce1
jungjihyoun <jihyoun0602@gmail.com>
2024-05-26 06:12:50 +0900

    fix : date2

--- src/shared/components/RNTSTimePicker/index.tsx
+++ src/shared/components/RNTSTimePicker/index.tsx
@@ -37,7 +37,7 @@ export default function RNTSTimePicker(props: Props) {
       setAppointment((prev) => {
         return {
           ...prev,
-          HHMM: formattedTime,
+          HHMM: `${hour}:${minute}`,
         };
       });
 
