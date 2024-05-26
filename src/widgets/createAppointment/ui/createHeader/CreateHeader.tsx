70b806c32e4fcd265f094fd9d960c51cfb5a6dcf
jungjihyoun <jihyoun0602@gmail.com>
2024-05-26 12:53:43 +0900

    qa 2

--- src/widgets/createAppointment/ui/createHeader/CreateHeader.tsx
+++ src/widgets/createAppointment/ui/createHeader/CreateHeader.tsx
@@ -1,7 +1,8 @@
-import { useNavigate } from 'react-router-dom';
-import './createHeader.scss';
 import chevronLeft from '@/assets/chevronLeft.svg';
 import Description from '@/entities/appointment/ui/description/Description';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './createHeader.scss';
 
 interface CreateHeaderProps {
   title: string;
@@ -10,10 +11,23 @@ interface CreateHeaderProps {
 
 const CreateHeader = ({ title, description }: CreateHeaderProps) => {
   const navigate = useNavigate();
+  const [nav, setNav] = useState('');
+
+  useEffect(() => {
+    if (window.location.pathname.includes('/create/schedule')) {
+      setNav('/appointment/create/type');
+    } else {
+      setNav('/');
+    }
+  }, [window.location.pathname]);
 
   return (
     <div className="appointment_create_header_container">
-      <button onClick={() => navigate(-1)} className="back_icon">
+      <button
+        onClick={() => {
+          navigate(nav);
+        }}
+        className="back_icon">
         <img src={chevronLeft} alt="chevronLeft" />
       </button>
       <Description title={title} description={description} />
