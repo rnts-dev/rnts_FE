a582e8656a097fd77a983aa92a15f31625f9488f
striker1826 <alstjq123579@daum.net>
2024-07-28 16:34:36 +0900

    fix: svg 파일 잘못된 import 경로 수정

--- src/components/policy/policyAccept/PolicyAccept.tsx
+++ src/components/policy/policyAccept/PolicyAccept.tsx
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom';
 import * as S from './policyAccept.styled';
 import { Dispatch } from 'react';
 import { SetStateAction } from 'jotai';
+import check from '@/assets/auth/check.svg';
+import checkbox from '@/assets/checkbox.svg';
+import arrowRight from '@/assets/arrow-right.svg';
 
 interface Props {
   POLICY_LINK?: string;
@@ -15,24 +18,24 @@ const PolicyAccept = ({ POLICY_LINK, description, isCheck, onChangeCheck }: Prop
     <S.PolicyDetail>
       {isCheck ? (
         <button type="button" onClick={() => onChangeCheck((prev: boolean) => !prev)}>
-          <img width={16} height={16} src="/src/assets/check.svg" alt="check" />
+          <img width={16} height={16} src={check} alt="check" />
         </button>
       ) : (
         <button type="button" onClick={() => onChangeCheck((prev: boolean) => !prev)}>
-          <img width={16} height={16} src="/src/assets/checkbox.svg" alt="checkbox" />
+          <img width={16} height={16} src={checkbox} alt="checkbox" />
         </button>
       )}
 
       {POLICY_LINK && (
         <Link to={POLICY_LINK} target="_blank">
           <span>{description}</span>
-          <img width={24} height={24} src="/src/assets/arrow-right.svg" alt="arrow-right" />
+          <img width={24} height={24} src={arrowRight} alt="arrow-right" />
         </Link>
       )}
       {!POLICY_LINK && (
         <>
           <span>{description}</span>
-          <img width={24} height={24} src="/src/assets/arrow-right.svg" alt="arrow-right" />
+          <img width={24} height={24} src={arrowRight} alt="arrow-right" />
         </>
       )}
     </S.PolicyDetail>
