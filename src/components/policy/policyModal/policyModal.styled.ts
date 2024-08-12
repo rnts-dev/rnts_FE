78c88d83401005b43060148a56b89d85d43708d1
striker1826 <alstjq123579@daum.net>
2024-08-12 16:23:49 +0900

    fix: 모달 confirm 버튼 화면에 가려지는 현상 수정

--- src/components/policy/policyModal/policyModal.styled.ts
+++ src/components/policy/policyModal/policyModal.styled.ts
@@ -39,7 +39,7 @@ export const PolicyModal = styled.div<IsOpen>`
   background: #fff;
   width: 100%;
   position: absolute;
-  bottom: 0;
+  bottom: 110px;
   left: 0;
   z-index: 1000;
   padding: 36px 24px 40px;
